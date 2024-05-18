1adfd3cd017e350a9e01c2fc2045be06111c74b0
devhak <devhak.enzo@gmail.com>
2024-05-18 20:37:18 +0900

    userresponse record 변경

--- flow/src/main/java/com/fastcampus/flow/controller/UserQueueController.java
+++ flow/src/main/java/com/fastcampus/flow/controller/UserQueueController.java
@@ -1,5 +1,7 @@
 package com.fastcampus.flow.controller;
 
+import com.fastcampus.flow.dto.RegisterUserResponse;
+import com.fastcampus.flow.service.UserQueueService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,11 +11,12 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api/v1/queue")
 public class UserQueueController {
-    private final UserQueueService userQueueService;
+    private static UserQueueService userQueueService;
 
     @PostMapping("")
-    public Mono<?> reqisterUser(@RequestParam(name = "user_id")Long userId){
-        return userQueueService.regisetWaitQueue(userId);''
+    public Mono<RegisterUserResponse> reqisterUser(@RequestParam(name = "user_id")Long userId){
+        return userQueueService.registerWaitQueue(userId)
+                .map(RegisterUserResponse::new);
     }
 
 
--- flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
+++ flow/src/main/java/com/fastcampus/flow/service/UserQueueService.java
@@ -5,6 +5,8 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.time.Instant;
+
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
@@ -12,10 +14,16 @@ public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     //대기열 등록
-    public Mono<?> registerWaitQueue(final Long userId){
+    public Mono<Long> registerWaitQueue(final Long userId) {
         //redis sortedset
         //-key : userId
         // - value : unix timestamp
-        
+        var unixTimeStamp = Instant.now().getEpochSecond();
+        return reactiveRedisTemplate.opsForZSet().add("user-queue", userId.toString(), unixTimeStamp)
+                .filter(i -> i)
+                .switchIfEmpty(Mono.error(new Exception("already register user...")))
+                .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank("user-queue", userId.toString()))
+                .map(i -> i >= 0 ? i + 1 : i);
+
     }
 }
